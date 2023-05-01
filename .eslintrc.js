db111985091667095228f784a2e1fa146b1c72ef
hongseungah <gmm117@naver.com>
2023-05-01 19:46:36 +0900

    ENV feat: 환경설정
     - dartSass 적용

--- .eslintrc.js
+++ .eslintrc.js
@@ -27,6 +27,11 @@ module.exports = {
   parser: '@babel/eslint-parser',
 
   rules: {
+    'no-var': 'warn', // var 금지
+    'no-multiple-empty-lines': 'warn', // 여러 줄 공백 금지
+    'no-nested-ternary': 'warn', // 중첩 삼항 연산자 금지
+    'no-console': 'warn', // console.log() 금지
+    'no-unused-vars': 'warn', // 사용하지 않는 변수 금지
     'linebreak-style': 0,
     'import/prefer-default-export': 0,
     'prettier/prettier': 0,
@@ -36,6 +41,14 @@ module.exports = {
     'import/no-extraneous-dependencies': 0, // 테스트 또는 개발환경을 구성하는 파일에서는 devDependency 사용을 허용
     'no-shadow': 0,
     'react/prop-types': 0,
+    'react/self-closing-comp': 'warn', // 셀프 클로징 태그 가능하면 적용
+    'react/destructuring-assignment': 'warn', // state, prop 등에 구조분해 할당 적용
+    'react/jsx-pascal-case': 'warn', // 컴포넌트 이름은 PascalCase로
+    'react/no-direct-mutation-state': 'warn', // state 직접 수정 금지
+    'react/jsx-no-useless-fragment': 'warn', // 불필요한 fragment 금지
+    'react/no-unused-state': 'warn', // 사용되지 않는 state
+    'react/jsx-key': 'warn', // 반복문으로 생성하는 요소에 key 강제
+    'react/jsx-curly-brace-presence': 'warn', // jsx 내 불필요한 중괄호 금지
     'react/jsx-filename-extension': [
       2,
       { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
