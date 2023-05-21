978059b536b1b0c35bff2e502eea20e686e00a5b
hongseungah <gmm117@naver.com>
2023-05-21 18:20:44 +0900

    CloneCode1 feat: 클론코딩1
      - 라우터 링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, NavLink } from 'react-router-dom';
 import TossClone from './TossClone/TossClone';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/toss" element={<TossClone />} />
-      <Route path="/" element={<div>Clone Coding...</div>} />
-    </Routes>
+    <>
+      <NavLink to="/toss">토스</NavLink>
+      <Routes>
+        <Route path="/toss" element={<TossClone />} />
+        <Route path="/" element={<div>Clone Coding...</div>} />
+      </Routes>
+    </>
   );
 }
 
